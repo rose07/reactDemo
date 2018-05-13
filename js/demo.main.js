2d2877e9fc02648c6c7a9626c2677f64dac30ae4
rose07 <1223610629@qq.com>
2018-05-13 11:25:22 +0800

    react demo

---
+++ js/demo.main.js
@@ -0,0 +1,19 @@
+// class HelloMessage extends React.Component {
+//     render() {
+//         return <div>
+//                     <h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1>
+//                     <h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1>
+//                     <h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1>
+//                     <h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1>
+//                     <h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1><h1>小胡</h1>
+//                </div>;
+//     }
+// }
+
+
+//   ReactDOM.render(
+    
+    
+//     <HelloMessage />,
+//     document.getElementById('example')
+//   );
